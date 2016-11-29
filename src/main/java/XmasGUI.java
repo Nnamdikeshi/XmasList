b9fc510a8ce769e8d4c31cf67697e128dcb52c06
Nnamdi <nnamdinj@aol.com>
2016-11-29 13:46:46 -0600

    GUI created

---
+++ src/main/java/XmasGUI.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by Nnamdi on 11/29/2016.
+ */
+public class XmasGUI {
+    private JTable needTable;
+    private JTextPane NEEDTABLETextPane1;
+    private JTextPane WANTTABLETextPane;
+    private JTable wantTable;
+    private JButton button1;
+    private JButton button2;
+    private JButton exitButton;
+    private JTextPane welcomeToXmasListTextPane;
+}
